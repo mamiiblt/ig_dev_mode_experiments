b23043f9b1b951f71eac8b491515fffabe3a49c7
M. Ali BULUT <70213359+mamiiblt@users.noreply.github.com>
2023-10-14 17:19:28 +0300

    v2

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Instagram Developer Options',
+    description: 'Instagram developer options documentations.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: '',
       github: 'mamiiblt',
@@ -15,28 +16,42 @@ export default defineAppConfig({
 
       instagram: 'mamiiblt'
     },
+
     github: {
       dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      branch: 'maces',
+      repo: 'ig_dev_mode_experiments',
+      owner: 'mamiiblt',
+      edit: false
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: false,
-      showLinkIcon: true,
+      showLinkIcon: false,
       exclude: [],
       fluid: true,
-      title: 'IGDevMode Docs'
+      title: 'Instagram Developer Options'
+    },
+
+    footer: {
+      credits: {
+        icon: '',
+        text: 'Writed by mamiiblt.',
+        href: 'https://instagram.com/mamiiblt'
+      },
+
+      fluid: true
     }
   }
 })
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,18 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    primary: {
+      '50': '#F3FAF7',
+      '100': '#DEF7EC',
+      '200': '#BCF0DA',
+      '300': '#84E1BC',
+      '400': '#31C48D',
+      '500': '#0E9F6E',
+      '600': '#057A55',
+      '700': '#046C4E',
+      '800': '#03543F',
+      '900': '#014737'
+    }
+  }
 })
