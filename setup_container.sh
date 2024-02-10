d67933b37a72bf95ae970c7025eecbac60548820
M. Ali BULUT <70213359+mamiiblt@users.noreply.github.com>
2024-02-10 23:35:20 +0300

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -1,2 +1,4 @@
 echo 'Downloading android-studio'
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.28/android-studio-2023.1.1.28-linux.tar.gz 
+echo 'Exracting android-studio'
+tar -xf android-studio-2023.1.1.28-linux.tar.gz
