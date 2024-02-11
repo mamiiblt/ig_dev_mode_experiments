4bb1d18f60c4d1b26ac543eaedb6b298a991c34e
M. Ali BULUT <70213359+mamiiblt@users.noreply.github.com>
2024-02-11 15:35:30 +0300

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -1,21 +1,16 @@
-# echo 'Downloading android-studio'
-# wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.28/android-studio-2023.1.1.28-linux.tar.gz 
-# echo 'Exracting android-studio'
-# tar -xf android-studio-2023.1.1.28-linux.tar.gz
-
-echo '1 > Installing graphical envinmorent and chrome_remote_desktop'
-
-sudo apt-get update | sudo apt-get upgrade
-sudo apt install xvfb xfce4 xfce4-goodies mpv firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome-remote-desktop*
-sudo apt --fix-broken install --assume-yes
-rm chrome-remote-desktop*
+echo '1 > Configure keyboard into tr'
+setxkbmap tr
 
 echo '2 > Install brave-browser'
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt install brave-browser
+
+echo '3 > Download android-studio into /tmp/'
+cd /tmp/
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.28/android-studio-2023.1.1.28-linux.tar.gz 
+tar -xf android-studio-2023.1.1.28-linux.tar.gz
+
+
 
 
