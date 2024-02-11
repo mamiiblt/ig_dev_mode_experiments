c6b664cd1b9f4dbac6a35663f32d4664e2ae6fe9
M. Ali BULUT <70213359+mamiiblt@users.noreply.github.com>
2024-02-11 14:53:21 +0300

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -1,4 +1,12 @@
-echo 'Downloading android-studio'
-wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.28/android-studio-2023.1.1.28-linux.tar.gz 
-echo 'Exracting android-studio'
-tar -xf android-studio-2023.1.1.28-linux.tar.gz
+# echo 'Downloading android-studio'
+# wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.28/android-studio-2023.1.1.28-linux.tar.gz 
+# echo 'Exracting android-studio'
+# tar -xf android-studio-2023.1.1.28-linux.tar.gz
+echo '1 > Installing graphical envinmorent and chrome_remote_desktop'
+
+sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
+# Download Chrome Remote Desktop, install it, then delete downloaded .deb package
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome-remote-desktop*
+sudo apt --fix-broken install --assume-yes
+rm chrome-remote-desktop*
